220502bc036daea515b242bd42b2d3a6369f834b
Maria Hansen <maria.hansen@mni.thm.de>
2024-07-06 11:47:50 +0200

    test: test serialization of a set policy with two rules

--- tests/odrl/json_serializer_test.rs
+++ tests/odrl/json_serializer_test.rs
@@ -39,4 +39,45 @@ mod json_ld_serializer_test {
         );
     }
 
+    #[test]
+    fn test_set_policy_with_two_rules_serialize() {
+        let action_1 = Action::new("play", None, None, vec![]);
+        let mut asset_1 = Asset::default();
+        asset_1.uid = Some("https://example.com/asset:9898.movie".to_string());
+        let mut permission_1 = Permission::default();
+        permission_1.target = asset_1;
+        permission_1.action = action_1;
+
+        let action_2 = Action::new("use", None, None, vec![]);
+        let mut asset_2 = Asset::default();
+        asset_2.uid = Some("https://example.com/asset:1212".to_string());
+        let mut permission_2 = Permission::default();
+        permission_2.target = asset_2;
+        permission_2.action = action_2;
+
+        let mut policy = SetPolicy::default();
+        policy.uid = "https://example.com/policy:1010".to_string();
+        policy.rules = vec![Rule::Permission(permission_1), Rule::Permission(permission_2)];
+
+        let serialized_policy = serde_json::to_string_pretty(&policy).unwrap();
+        let expected_policy = r#"
+        {
+            "@type": "Set",
+            "uid": "https://example.com/policy:1010",
+            "permission": [{
+            "target": "https://example.com/asset:9898.movie",
+            "action": "play"
+            },
+            {
+            "target": "https://example.com/asset:1212",
+            "action": "use"
+            }]
+        }"#.to_string();
+
+        assert_eq!(
+            serialized_policy.parse::<serde_json::Value>().unwrap(),
+            expected_policy.parse::<serde_json::Value>().unwrap()
+        );
+    }
+
 }
