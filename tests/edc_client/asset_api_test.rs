b08d92ee0a3f3bbe6f5edd23689dcbe8d6dd9006
Maria Hansen <maria.hansen@mni.thm.de>
2024-07-10 13:05:57 +0200

    test: add testing of get_asset

--- tests/edc_client/asset_api_test.rs
+++ tests/edc_client/asset_api_test.rs
@@ -1,7 +1,7 @@
 mod common;
 
 #[cfg(test)]
-mod asset_api_test {
+mod asset_api_create_test {
 
     extern crate edc_api;
     extern crate edc_client;
@@ -94,6 +94,68 @@ mod asset_api_test {
             _ => panic!("Expected Status Code 409, because an asset with that ID already exists"),
         }
 
+    }
+
+}
+
+#[cfg(test)]
+mod asset_api_get_test {
+
+    extern crate edc_api;
+    extern crate edc_client;
+
+    use crate::common::setup_provider_configuration;
+    use edc_api::{AssetInput, DataAddress};
+    use edc_client::{asset_api, Error};
+
+    use uuid::Uuid;
+
+    #[tokio::test]
+    async fn test_get_asset() {
+        let configuration = setup_provider_configuration();
+
+        let id = Uuid::new_v4().to_string();
+        let mut properties = std::collections::HashMap::new();
+        properties.insert("name".to_string(), serde_json::Value::String("test".to_string()));
+        properties.insert("foo".to_string(), serde_json::Value::String("bar".to_string()));
+        let mut data_address = DataAddress::default();
+        data_address.r#type = Some("https://w3id.org/edc/v0.0.1/ns/DataAddress".to_string());
+
+        let mut asset = AssetInput::default();
+        asset.at_id = Some(id.clone());
+        asset.data_address = Box::new(data_address.clone());
+        asset.properties = properties.clone();
+
+        let _ = asset_api::create_asset(&configuration, Some(asset)).await.unwrap();
+
+        let response = asset_api::get_asset(&configuration, &id.clone()).await.unwrap();
+
+        // Check for correct properties
+        assert!(response.properties.is_some());
+        assert_eq!(response.properties.clone().unwrap().get("foo").unwrap(), &serde_json::Value::String("bar".to_string()));
+        assert_eq!(response.properties.clone().unwrap().get("name").unwrap(), &serde_json::Value::String("test".to_string()));
+
+        // Check for correct data address
+        assert!(response.data_address.is_some());
+        assert_eq!(response.data_address.unwrap(), Box::new(data_address.clone()));
+
+    }
+
+    #[tokio::test]
+    async fn test_get_asset_with_unknown_id() {
+        let configuration = setup_provider_configuration();
+
+        let id = Uuid::new_v4().to_string();
+
+        let response = asset_api::get_asset(&configuration, &id.clone()).await;
+
+        assert!(response.is_err());
+        match response {
+            Err(Error::ResponseError(response)) => {
+                assert_eq!(response.status, reqwest::StatusCode::NOT_FOUND);
+            },
+            _ => panic!("Expected Status Code 404, because no asset with that ID exists"),
+        }
     }
 
 }
