1c2729a77ff9938bb725900312a338ec6c2dd678
Maria Hansen <maria.hansen@mni.thm.de>
2024-07-25 11:22:21 +0200

    feat: add various fields to the transfer process struct so that they can be defined more precisely

--- crates/edc_api/src/transfer_process.rs
+++ crates/edc_api/src/transfer_process.rs
@@ -7,6 +7,9 @@
  *
  */
 
+use serde_with::{formats::PreferMany, serde_as, OneOrMany};
+
+#[serde_as]
 #[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
 pub struct TransferProcess {
     #[serde(rename = "@context")]
@@ -15,9 +18,14 @@ pub struct TransferProcess {
     pub at_type: Option<String>,
     #[serde(rename = "@id", skip_serializing_if = "Option::is_none")]
     pub at_id: Option<String>,
-    #[serde(rename = "callbackAddresses", skip_serializing_if = "Option::is_none")]
-    pub callback_addresses: Option<Vec<crate::CallbackAddress>>,
-    #[serde(rename = "contractAgreementId", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "correlationId", skip_serializing_if = "Option::is_none")]
+    pub correlation_id: Option<String>,
+    #[serde(rename = "callbackAddresses", skip_serializing_if = "Vec::is_empty")]
+    #[serde_as(deserialize_as = "OneOrMany<_, PreferMany>")]
+    pub callback_addresses: Vec<crate::CallbackAddress>,
+    #[serde(rename = "assetId", skip_serializing_if = "Option::is_none")]
+    pub asset_id: Option<String>,
+    #[serde(rename = "contractId", skip_serializing_if = "Option::is_none")]
     pub contract_agreement_id: Option<String>,
     #[serde(rename = "counterPartyAddress", skip_serializing_if = "Option::is_none")]
     pub counter_party_address: Option<String>,
@@ -33,21 +41,25 @@ pub struct TransferProcess {
     pub protocol: Option<String>,
     #[serde(rename = "state", skip_serializing_if = "Option::is_none")]
     pub state: Option<String>,
+    #[serde(rename = "transferType", skip_serializing_if = "Option::is_none")]
+    pub transfer_type: Option<String>,
     #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub r#type: Option<RHashType>,
 }
 
 impl TransferProcess {
 
-    pub fn new(context: std::collections::HashMap<String, serde_json::Value>, at_type: Option<String>, at_id: Option<String>, callback_addresses: Option<Vec<crate::CallbackAddress>>,
-               contract_agreement_id: Option<String>, counter_party_address: Option<String>, counter_party_id: Option<String>, data_destination: Option<Box<crate::DataAddress>>,
+    pub fn new(context: std::collections::HashMap<String, serde_json::Value>, at_type: Option<String>, at_id: Option<String>, correlation_id: Option<String>, callback_addresses: Vec<crate::CallbackAddress>,
+               asset_id: Option<String>, contract_agreement_id: Option<String>, counter_party_address: Option<String>, counter_party_id: Option<String>, data_destination: Option<Box<crate::DataAddress>>,
                error_detail: Option<String>, private_properties: Option<::std::collections::HashMap<String, String>>, protocol: Option<String>, state: Option<String>,
-               r#type: Option<RHashType>) -> TransferProcess {
+               transfer_type: Option<String>, r#type: Option<RHashType>) -> TransferProcess {
         TransferProcess {
             context,
             at_type,
             at_id,
+            correlation_id,
             callback_addresses,
+            asset_id,
             contract_agreement_id,
             counter_party_address,
             counter_party_id,
@@ -56,6 +68,7 @@ impl TransferProcess {
             private_properties,
             protocol,
             state,
+            transfer_type,
             r#type,
         }
     }
@@ -65,7 +78,9 @@ impl TransferProcess {
             context: std::collections::HashMap::from([("@vocab".to_string(), serde_json::Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string()))]),
             at_type: Some("TransferProcess".to_string()),
             at_id: None,
-            callback_addresses: None,
+            correlation_id: None,
+            callback_addresses: Vec::new(),
+            asset_id: None,
             contract_agreement_id: None,
             counter_party_address: None,
             counter_party_id: None,
@@ -74,6 +89,7 @@ impl TransferProcess {
             private_properties: None,
             protocol: None,
             state: None,
+            transfer_type: None,
             r#type: None,
         }
     }
