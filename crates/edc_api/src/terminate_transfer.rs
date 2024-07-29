13694408ebab24041998ca3e1e59f173e58b1adc
Maria Hansen <maria.hansen@mni.thm.de>
2024-07-29 13:17:19 +0200

    feat: add provider id and transfer id to the terminate transfer struct

--- crates/edc_api/src/terminate_transfer.rs
+++ crates/edc_api/src/terminate_transfer.rs
@@ -15,15 +15,21 @@ pub struct TerminateTransfer {
     pub at_type: Option<String>,
     #[serde(rename = "reason", skip_serializing_if = "Option::is_none")]
     pub reason: Option<String>,
+    #[serde(rename = "providerPid", skip_serializing_if = "Option::is_none")]
+    pub provider_id: Option<String>,
+    #[serde(rename = "id", skip_serializing_if = "Option::is_none")]
+    pub transfer_id: Option<String>,
 }
 
 impl TerminateTransfer {
 
-    pub fn new(context: std::collections::HashMap<String, serde_json::Value>, at_type: Option<String>, reason: Option<String>) -> TerminateTransfer {
+    pub fn new(context: std::collections::HashMap<String, serde_json::Value>, at_type: Option<String>, reason: Option<String>, provider_id: Option<String>, transfer_id: Option<String>) -> TerminateTransfer {
         TerminateTransfer {
             context,
             at_type,
             reason,
+            provider_id,
+            transfer_id,
         }
     }
 
@@ -32,6 +38,8 @@ impl TerminateTransfer {
             context: std::collections::HashMap::from([("@vocab".to_string(), serde_json::Value::String("https://w3id.org/edc/v0.0.1/ns/".to_string()))]),
             at_type: Some("TerminateTransfer".to_string()),
             reason: None,
+            provider_id: None,
+            transfer_id: None,
         }
     }
 
