13832d4966e3980fd6684f6bf2fd4abf96cef4d4
Maria Hansen <maria.hansen@mni.thm.de>
2024-08-26 13:50:57 +0200

    refactor: remove struct definitions to a subdirectory contract negotiation for better project structure

--- crates/dsp_api/src/lib.rs
+++ crates/dsp_api/src/lib.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate serde_derive;
 
-pub mod contract_request_message;
-pub use self::contract_request_message::ContractRequestMessage;
-pub mod contract_offer_message;
-pub use self::contract_offer_message::ContractOfferMessage;
+pub mod contract_negotiation {
+    pub mod contract_request_message;
+    pub use self::contract_request_message::ContractRequestMessage;
+    pub mod contract_offer_message;
+    pub use self::contract_offer_message::ContractOfferMessage;
+}
