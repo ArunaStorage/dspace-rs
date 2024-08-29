e28149c88b142d8a701522afc8bde034ef797607
Maria Hansen <maria.hansen@mni.thm.de>
2024-08-29 13:09:10 +0200

    fix: fix string concatenation to the correct namespaces

--- crates/aruna_connector/src/dsp/ld_properties_and_type_names.rs
+++ crates/aruna_connector/src/dsp/ld_properties_and_type_names.rs
@@ -1,56 +1,54 @@
-use odrl::name_spaces::{DCAT_NS, DCTERMS_NS, DSPACE_NS, EDC_NS, ODRL_NS};
-
 // DCAT
-pub const DCAT_CATALOG_TYPE: &'static str = concat!(DCAT_NS, "Catalog");
-pub const DCAT_DATASET_TYPE: &'static str = concat!(DCAT_NS, "Dataset");
-pub const DCAT_DISTRIBUTION_TYPE: &'static str = concat!(DCAT_NS, "Distribution");
-pub const DCAT_DATA_SERVICE_TYPE: &'static str = concat!(DCAT_NS, "DataService");
-pub const DCAT_DATA_SERVICE_ATTRIBUTE: &'static str = concat!(DCAT_NS, "service");
-pub const DCAT_DATASET_ATTRIBUTE: &'static str = concat!(DCAT_NS, "dataset");
-pub const DCAT_DISTRIBUTION_ATTRIBUTE: &'static str = concat!(DCAT_NS, "distribution");
-pub const DCAT_ACCESS_SERVICE_ATTRIBUTE: &'static str = concat!(DCAT_NS, "accessService");
-pub const DCAT_ENDPOINT_URL_ATTRIBUTE: &'static str = concat!(DCAT_NS, "endpointUrl");
-pub const DCAT_ENDPOINT_DESCRIPTION_ATTRIBUTE: &'static str = concat!(DCAT_NS, "endpointDescription");
+pub const DCAT_CATALOG_TYPE: &'static str = concat!("http://www.w3.org/ns/dcat#", "Catalog");
+pub const DCAT_DATASET_TYPE: &'static str = concat!("http://www.w3.org/ns/dcat#", "Dataset");
+pub const DCAT_DISTRIBUTION_TYPE: &'static str = concat!("http://www.w3.org/ns/dcat#", "Distribution");
+pub const DCAT_DATA_SERVICE_TYPE: &'static str = concat!("http://www.w3.org/ns/dcat#", "DataService");
+pub const DCAT_DATA_SERVICE_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/dcat#", "service");
+pub const DCAT_DATASET_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/dcat#", "dataset");
+pub const DCAT_DISTRIBUTION_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/dcat#", "distribution");
+pub const DCAT_ACCESS_SERVICE_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/dcat#", "accessService");
+pub const DCAT_ENDPOINT_URL_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/dcat#", "endpointUrl");
+pub const DCAT_ENDPOINT_DESCRIPTION_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/dcat#", "endpointDescription");
 
 // EDC
-pub const EDC_CREATED_AT: &'static str = concat!(EDC_NS, "createdAt");
+pub const EDC_CREATED_AT: &'static str = concat!("https://w3id.org/edc/v0.0.1/ns/", "createdAt");
 
 // DCTERMS
-pub const DCTERMS_FORMAT_ATTRIBUTE: &'static str = concat!(DCTERMS_NS, "format");
-pub const DCTERMS_TERMS_ATTRIBUTE: &'static str = concat!(DCTERMS_NS, "terms");
-pub const DCTERMS_ENDPOINT_URL_ATTRIBUTE: &'static str = concat!(DCTERMS_NS, "endpointUrl");
+pub const DCTERMS_FORMAT_ATTRIBUTE: &'static str = concat!("http://purl.org/dc/terms/", "format");
+pub const DCTERMS_TERMS_ATTRIBUTE: &'static str = concat!("http://purl.org/dc/terms/", "terms");
+pub const DCTERMS_ENDPOINT_URL_ATTRIBUTE: &'static str = concat!("http://purl.org/dc/terms/", "endpointUrl");
 
 // ODRL
-pub const ODRL_POLICY_ATTRIBUTE: &'static str = concat!(ODRL_NS, "hasPolicy");
-pub const ODRL_POLICY_TYPE_SET: &'static str = concat!(ODRL_NS, "Set");
-pub const ODRL_POLICY_TYPE_OFFER: &'static str = concat!(ODRL_NS, "Offer");
-pub const ODRL_POLICY_TYPE_AGREEMENT: &'static str = concat!(ODRL_NS, "Agreement");
-pub const ODRL_CONSTRAINT_TYPE: &'static str = concat!(ODRL_NS, "Constraint");
-pub const ODRL_LOGICAL_CONSTRAINT_TYPE: &'static str = concat!(ODRL_NS, "LogicalConstraint");
-pub const ODRL_TARGET_ATTRIBUTE: &'static str = concat!(ODRL_NS, "target");
-pub const ODRL_ASSIGNEE_ATTRIBUTE: &'static str = concat!(ODRL_NS, "assignee");
-pub const ODRL_ASSIGNER_ATTRIBUTE: &'static str = concat!(ODRL_NS, "assigner");
-pub const ODRL_PERMISSION_ATTRIBUTE: &'static str = concat!(ODRL_NS, "permission");
-pub const ODRL_PROHIBITION_ATTRIBUTE: &'static str = concat!(ODRL_NS, "prohibition");
-pub const ODRL_OBLIGATION_ATTRIBUTE: &'static str = concat!(ODRL_NS, "obligation");
-pub const ODRL_ACTION_ATTRIBUTE: &'static str = concat!(ODRL_NS, "action");
-pub const ODRL_ACTION_TYPE_ATTRIBUTE: &'static str = concat!(ODRL_NS, "type");
-pub const ODRL_CONSEQUENCE_ATTRIBUTE: &'static str = concat!(ODRL_NS, "consequence");
-pub const ODRL_REMEDY_ATTRIBUTE: &'static str = concat!(ODRL_NS, "remedy");
-pub const ODRL_INCLUDED_IN_ATTRIBUTE: &'static str = concat!(ODRL_NS, "includedIn");
-pub const ODRL_REFINEMENT_ATTRIBUTE: &'static str = concat!(ODRL_NS, "refinement");
-pub const ODRL_CONSTRAINT_ATTRIBUTE: &'static str = concat!(ODRL_NS, "constraint");
-pub const ODRL_LEFT_OPERAND_ATTRIBUTE: &'static str = concat!(ODRL_NS, "leftOperand");
-pub const ODRL_OPERATOR_TYPE: &'static str = concat!(ODRL_NS, "Operator");
-pub const ODRL_OPERATOR_ATTRIBUTE: &'static str = concat!(ODRL_NS, "operator");
-pub const ODRL_RIGHT_OPERAND_ATTRIBUTE: &'static str = concat!(ODRL_NS, "rightOperand");
-pub const ODRL_DUTY_ATTRIBUTE: &'static str = concat!(ODRL_NS, "duty");
-pub const ODRL_AND_CONSTRAINT_ATTRIBUTE: &'static str = concat!(ODRL_NS, "and");
-pub const ODRL_AND_SEQUENCE_CONSTRAINT_ATTRIBUTE: &'static str = concat!(ODRL_NS, "andSequence");
-pub const ODRL_OR_CONSTRAINT_ATTRIBUTE: &'static str = concat!(ODRL_NS, "or");
-pub const ODRL_XONE_CONSTRAINT_ATTRIBUTE: &'static str = concat!(ODRL_NS, "xone");
-pub const ODRL_USE_ACTION_ATTRIBUTE: &'static str = concat!(ODRL_NS, "use");
-pub const ODRL_PROFILE_ATTRIBUTE: &'static str = concat!(ODRL_NS, "profile");
+pub const ODRL_POLICY_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "hasPolicy");
+pub const ODRL_POLICY_TYPE_SET: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "Set");
+pub const ODRL_POLICY_TYPE_OFFER: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "Offer");
+pub const ODRL_POLICY_TYPE_AGREEMENT: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "Agreement");
+pub const ODRL_CONSTRAINT_TYPE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "Constraint");
+pub const ODRL_LOGICAL_CONSTRAINT_TYPE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "LogicalConstraint");
+pub const ODRL_TARGET_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "target");
+pub const ODRL_ASSIGNEE_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "assignee");
+pub const ODRL_ASSIGNER_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "assigner");
+pub const ODRL_PERMISSION_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "permission");
+pub const ODRL_PROHIBITION_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "prohibition");
+pub const ODRL_OBLIGATION_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "obligation");
+pub const ODRL_ACTION_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "action");
+pub const ODRL_ACTION_TYPE_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "type");
+pub const ODRL_CONSEQUENCE_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "consequence");
+pub const ODRL_REMEDY_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "remedy");
+pub const ODRL_INCLUDED_IN_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "includedIn");
+pub const ODRL_REFINEMENT_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "refinement");
+pub const ODRL_CONSTRAINT_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "constraint");
+pub const ODRL_LEFT_OPERAND_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "leftOperand");
+pub const ODRL_OPERATOR_TYPE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "Operator");
+pub const ODRL_OPERATOR_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "operator");
+pub const ODRL_RIGHT_OPERAND_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "rightOperand");
+pub const ODRL_DUTY_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "duty");
+pub const ODRL_AND_CONSTRAINT_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "and");
+pub const ODRL_AND_SEQUENCE_CONSTRAINT_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "andSequence");
+pub const ODRL_OR_CONSTRAINT_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "or");
+pub const ODRL_XONE_CONSTRAINT_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "xone");
+pub const ODRL_USE_ACTION_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "use");
+pub const ODRL_PROFILE_ATTRIBUTE: &'static str = concat!("http://www.w3.org/ns/odrl/2/", "profile");
 
 // DSAPCE
-pub const DSPACE_PROPERTY_PARTICIPANT_ID: &'static str = concat!(DSPACE_NS, "participantId");
+pub const DSPACE_PROPERTY_PARTICIPANT_ID: &'static str = concat!("https://w3id.org/dspace/v0.8/", "participantId");
--- crates/aruna_connector/src/dsp/negotiation_properties_and_type_names.rs
+++ crates/aruna_connector/src/dsp/negotiation_properties_and_type_names.rs
@@ -1,36 +1,34 @@
-use odrl::name_spaces::DSPACE_NS;
-
 // types
-pub const DSPACE_TYPE_CONTRACT_NEGOTIATION: &'static str = concat!(DSPACE_NS, "ContractNegotiation");
-pub const DSPACE_TYPE_CONTRACT_NEGOTIATION_ERROR: &'static str = concat!(DSPACE_NS, "ContractNegotiationError");
+pub const DSPACE_TYPE_CONTRACT_NEGOTIATION: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractNegotiation");
+pub const DSPACE_TYPE_CONTRACT_NEGOTIATION_ERROR: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractNegotiationError");
 
 // messages
-pub const DSPACE_TYPE_CONTRACT_REQUEST_MESSAGE: &'static str = concat!(DSPACE_NS, "ContractRequestMessage");
-pub const DSPACE_TYPE_CONTRACT_OFFER_MESSAGE: &'static str = concat!(DSPACE_NS, "ContractOfferMessage");
-pub const DSPACE_TYPE_CONTRACT_NEGOTIATION_EVENT_MESSAGE: &'static str = concat!(DSPACE_NS, "ContractNegotiationEventMessage");
-pub const DSPACE_TYPE_CONTRACT_AGREEMENT_MESSAGE: &'static str = concat!(DSPACE_NS, "ContractAgreementMessage");
-pub const DSPACE_TYPE_CONTRACT_AGREEMENT_VERIFICATION_MESSAGE: &'static str = concat!(DSPACE_NS, "ContractAgreementVerificationMessage");
-pub const DSPACE_TYPE_CONTRACT_AGREEMENT_TERMINATION_MESSAGE: &'static str = concat!(DSPACE_NS, "ContractAgreementTerminationMessage");
+pub const DSPACE_TYPE_CONTRACT_REQUEST_MESSAGE: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractRequestMessage");
+pub const DSPACE_TYPE_CONTRACT_OFFER_MESSAGE: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractOfferMessage");
+pub const DSPACE_TYPE_CONTRACT_NEGOTIATION_EVENT_MESSAGE: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractNegotiationEventMessage");
+pub const DSPACE_TYPE_CONTRACT_AGREEMENT_MESSAGE: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractAgreementMessage");
+pub const DSPACE_TYPE_CONTRACT_AGREEMENT_VERIFICATION_MESSAGE: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractAgreementVerificationMessage");
+pub const DSPACE_TYPE_CONTRACT_AGREEMENT_TERMINATION_MESSAGE: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ContractAgreementTerminationMessage");
 
 // properties
-pub const DSPACE_PROPERTY_EVENT_TYPE: &'static str = concat!(DSPACE_NS, "eventType");
-pub const DSPACE_PROPERTY_AGREEMENT: &'static str = concat!(DSPACE_NS, "agreement");
-pub const DSPACE_PROPERTY_OFFER: &'static str = concat!(DSPACE_NS, "offer");
-pub const DSPACE_PROPERTY_TIMESTAMP: &'static str = concat!(DSPACE_NS, "timestamp");
+pub const DSPACE_PROPERTY_EVENT_TYPE: &'static str = concat!("https://w3id.org/dspace/v0.8/", "eventType");
+pub const DSPACE_PROPERTY_AGREEMENT: &'static str = concat!("https://w3id.org/dspace/v0.8/", "agreement");
+pub const DSPACE_PROPERTY_OFFER: &'static str = concat!("https://w3id.org/dspace/v0.8/", "offer");
+pub const DSPACE_PROPERTY_TIMESTAMP: &'static str = concat!("https://w3id.org/dspace/v0.8/", "timestamp");
 #[deprecated(note = "Deprecated since EDC v0.5.1")]
-pub const DSPACE_PROPERTY_CONSUMER_ID: &'static str = concat!(DSPACE_NS, "consumerId");
+pub const DSPACE_PROPERTY_CONSUMER_ID: &'static str = concat!("https://w3id.org/dspace/v0.8/", "consumerId");
 #[deprecated(note = "Deprecated since EDC v0.5.1")]
-pub const DSPACE_PROPERTY_PROVIDER_ID: &'static str = concat!(DSPACE_NS, "providerId");
+pub const DSPACE_PROPERTY_PROVIDER_ID: &'static str = concat!("https://w3id.org/dspace/v0.8/", "providerId");
 
 // event types
-pub const DSPACE_VALUE_NEGOTIATION_EVENT_TYPE_ACCEPTED: &'static str = concat!(DSPACE_NS, "ACCEPTED");
-pub const DSPACE_VALUE_NEGOTIATION_EVENT_TYPE_FINALIZED: &'static str = concat!(DSPACE_NS, "FINALIZED");
+pub const DSPACE_VALUE_NEGOTIATION_EVENT_TYPE_ACCEPTED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ACCEPTED");
+pub const DSPACE_VALUE_NEGOTIATION_EVENT_TYPE_FINALIZED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "FINALIZED");
 
 // negotiation states
-pub const DSPACE_VALUE_NEGOTIATION_STATE_REQUESTED: &'static str = concat!(DSPACE_NS, "REQUESTED");
-pub const DSPACE_VALUE_NEGOTIATION_STATE_OFFERED: &'static str = concat!(DSPACE_NS, "OFFERED");
-pub const DSPACE_VALUE_NEGOTIATION_STATE_ACCEPTED: &'static str = concat!(DSPACE_NS, "ACCEPTED");
-pub const DSPACE_VALUE_NEGOTIATION_STATE_AGREED: &'static str = concat!(DSPACE_NS, "AGREED");
-pub const DSPACE_VALUE_NEGOTIATION_STATE_VERIFIED: &'static str = concat!(DSPACE_NS, "VERIFIED");
-pub const DSPACE_VALUE_NEGOTIATION_STATE_FINALIZED: &'static str = concat!(DSPACE_NS, "FINALIZED");
-pub const DSPACE_VALUE_NEGOTIATION_STATE_TERMINATED: &'static str = concat!(DSPACE_NS, "TERMINATED");
+pub const DSPACE_VALUE_NEGOTIATION_STATE_REQUESTED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "REQUESTED");
+pub const DSPACE_VALUE_NEGOTIATION_STATE_OFFERED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "OFFERED");
+pub const DSPACE_VALUE_NEGOTIATION_STATE_ACCEPTED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "ACCEPTED");
+pub const DSPACE_VALUE_NEGOTIATION_STATE_AGREED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "AGREED");
+pub const DSPACE_VALUE_NEGOTIATION_STATE_VERIFIED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "VERIFIED");
+pub const DSPACE_VALUE_NEGOTIATION_STATE_FINALIZED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "FINALIZED");
+pub const DSPACE_VALUE_NEGOTIATION_STATE_TERMINATED: &'static str = concat!("https://w3id.org/dspace/v0.8/", "TERMINATED");
