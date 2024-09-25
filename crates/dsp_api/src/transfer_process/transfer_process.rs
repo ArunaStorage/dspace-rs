536a3f0b20dc73b94aeba71d6a3913eafd3db398
Maria Hansen <maria.hansen@mni.thm.de>
2024-09-25 16:59:07 +0200

    fix: fix deserialization of different types of transfer state

--- crates/dsp_api/src/transfer_process/transfer_process.rs
+++ crates/dsp_api/src/transfer_process/transfer_process.rs
@@ -6,6 +6,9 @@
  * The Transfer Process is an object returned by a Consumer or Provider indicating a successful state change happened.
  */
 
+use serde::{Deserialize, Deserializer, Serialize};
+use serde::de::{self, Unexpected};
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct TransferProcess {
     #[serde(rename = "@context")]
@@ -16,7 +19,7 @@ pub struct TransferProcess {
     pub provider_pid: String,
     #[serde(rename = "dspace:consumerPid")]
     pub consumer_pid: String,
-    #[serde(rename = "dspace:state")]
+    #[serde(rename = "dspace:state", deserialize_with = "deserialize_transfer_state_type")]
     pub state: TransferStateType,
 }
 
@@ -80,6 +83,41 @@ pub enum EDCTransferState {
     DeprovisioningRequested,
     #[serde(rename = "DEPROVISIONED")]
     Deprovisioned,
+}
+
+// Custom deserialization for TransferStateType
+fn deserialize_transfer_state_type<'de, D>(deserializer: D) -> Result<TransferStateType, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value: &str = Deserialize::deserialize(deserializer)?;
+    match value {
+        "dspace:REQUESTED" | "https://w3id.org/dspace/v0.8/REQUESTED" => Ok(TransferStateType::TransferState(TransferState::REQUESTED)),
+        "dspace:STARTED" | "https://w3id.org/dspace/v0.8/STARTED" => Ok(TransferStateType::TransferState(TransferState::STARTED)),
+        "dspace:TERMINATED" | "https://w3id.org/dspace/v0.8/TERMINATED" => Ok(TransferStateType::TransferState(TransferState::TERMINATED)),
+        "dspace:COMPLETED" | "https://w3id.org/dspace/v0.8/COMPLETED" => Ok(TransferStateType::TransferState(TransferState::COMPLETED)),
+        "dspace:SUSPENDED" | "https://w3id.org/dspace/v0.8/SUSPENDED" => Ok(TransferStateType::TransferState(TransferState::SUSPENDED)),
+        "INITIAL" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Initial)),
+        "PROVISIONING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Provisioning)),
+        "PROVISIONING_REQUESTED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::ProvisioningRequested)),
+        "PROVISIONED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Provisioned)),
+        "REQUESTING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Requesting)),
+        "REQUESTED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Requested)),
+        "STARTING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Starting)),
+        "STARTED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Started)),
+        "SUSPENDING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Suspending)),
+        "SUSPENDED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Suspended)),
+        "RESUMING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Resuming)),
+        "RESUMED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Resumed)),
+        "COMPLETING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Completing)),
+        "COMPLETED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Completed)),
+        "TERMINATING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Terminating)),
+        "TERMINATED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Terminated)),
+        "DEPROVISIONING" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Deprovisioning)),
+        "DEPROVISIONING_REQUESTED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::DeprovisioningRequested)),
+        "DEPROVISIONED" => Ok(TransferStateType::EDCTransferState(EDCTransferState::Deprovisioned)),
+        _ => Err(de::Error::invalid_value(Unexpected::Str(value), &"a valid state")),
+    }
 }
 
 impl TransferProcess {
